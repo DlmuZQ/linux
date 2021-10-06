431555068c77b4ff67dfe81b50d3dfa7ce4a2f23
qiang.zhang <zhangqiang10@100tal.com>
2021-10-06 16:06:06 +0800

    debug

--- backlog.cpp
+++ backlog.cpp
@@ -27,5 +27,33 @@ int main(int argc, char* argv[])
     int port = atoi(argv[2]);
     int backlog = atoi(argv[3]);
 
-    
+    //create socket
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sockfd > 0);
+
+    //set address 
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    //rename 
+    int ret = bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
+
+    //listen
+    ret = listen(sock, backlog);
+    assert(ret != -1);
+
+    //wait to quit
+    while (!stop)
+    {
+        printf(".....");
+        sleep(1);
+    }
+    //close
+    close(sockfd);
+    printf("quit...");
+    return 0;
 }
